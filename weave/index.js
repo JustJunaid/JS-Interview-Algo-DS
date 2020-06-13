87d9134e332ee2e744d0aeb3102f43177c581727
Junaid Chaudhary <junaidchoudhary210@gmail.com>
2020-06-13 21:55:31 +0530

    implemented test cases and started weave

---
+++ weave/index.js
@@ -0,0 +1,5 @@
+// --- Directions
+// Implement a 'peek' method in this Queue class.
+// Peek should return the last element (the next
+// one to be returned) from the queue *without*
+// removing it.
